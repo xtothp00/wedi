1ab5bd9e802e292c71f5ac837c4bc077ec2d9f2e
Peter Toth <ptoth@redhat.com>
2018-03-29 15:22:28 +0200

    trying the positional parameters

--- wedi.sh
+++ wedi.sh
@@ -2,7 +2,7 @@
 #   Interpretation of a wrapper script for text editor called wedi
 # for the purpose of the course IOS @ the FIT, BUT -- spring semester 2017.
 
-# readonly vars as constants
+# readonly vars as constants.
 POSIXLY_COORRECT=yes
 readonly EDI_0=1
 readonly EDI_1=2
@@ -12,80 +12,12 @@ readonly EDI_4=16
 readonly EDI_5=32
 readonly EDI_6=64
 readonly EDI_7=128
-#readonly EDITORS="ed ex vi vim emacs nano"
 readonly EDITORS=(\${VISUAL:-foo} \${EDITOR:-bar} nano emacs vim vi ex ed )
+
+# Status Register of the editors on the system.
 STAT_REG=0;
 
-#The init function
-init()
-{
-  # Check of the existence of the $WEDI_RC
-#  echo ${WEDI_RC?"\$WEDI_RC: paramter unset"}
-  if [ -z "${WEDI_RC:+"test"}" -a "${WEDI_RC+"test"}" = "test" ]; then
-    echo "\$WEDI_RC: paramter set but null" 1>&2
-    return 4
-  elif [ -z "${WEDI_RC:+"test"}" -a -z "${WEDI_RC+"test"}" ]; then
-    echo "\$WEDI_RC: paramter unset" 1>&2
-    return 3
-  fi
-
-  # Let's see the editors on the system. Shall we?
-##  if [ -n "${VISUAL}" -a check_editor_presence "${VISUAL}" ]; then
-#  if [ -n "${VISUAL}" ]; then if check_editor_presence "${VISUAL}"; then
-#    let "STAT_REG |= ${VIS_SET}"; fi
-#  elif [ -n "${EDITOR}" ]; then if check_editor_presence "${EDITOR}"; then
-#    let "STAT_REG |= ${EDI_SET}"; fi
-#  else
-#    scan_editors;
-#  fi
-  scan_editors;
-
-  return 0
-}
-
-check_editor_presence()
-{
-  if command -v ${1} 1>/dev/null 2>&2; then
-    return 0
-  else
-    return 1
-  fi
-
-  exit 1
-}
-
-scan_editors()
-{
-  echo "Scanning editors..."
-
-#  for i in $(echo ${EDITORS} | awk '{ for(i=0; i<NF; i++) print i }')
-  for i in {0..7}
-  do
-#    echo " *** Editor #${i} --" $(eval echo ${EDITORS[${i}]})
-#    check_editor_presence $(eval echo ${EDITORS[${i}]})
-#    echo $?
-#    command -v $(eval echo ${EDITORS[${i}]})
-#    echo $?
-    if check_editor_presence $(eval echo ${EDITORS[${i}]}); then
-      let "STAT_REG |= $(eval echo \${EDI_${i}})"
-    fi
-  done
-  printf "\nSTATUS REGISTER: %x\n\n" $STAT_REG
-  return 0
-}
-
-open_file()
-{
-  echo "Openning file ${1}..."
-
-  for i in {0..7}
-  do
-    if let "STAT_REG ^ (1 << ${i})"; then
-      eval echo ${EDITORS[${i}]}
-    fi
-  done
-}
-
+# An EXTRA help for the usage.
 show_help()
 {
   echo "NAME"
@@ -112,22 +44,96 @@ show_help()
   echo "       The editors are sorted in an "
 }
 
-#  original thoughts of the simplicity of getting the abolute path...
-# get_path()
-# {
-#   echo "$(cd "$(dirname "${1}")"; pwd)/$(basename "${1}")"
-# }
+#The init function
+init()
+{
+  # Check of the existence of the $WEDI_RC
+  if [ -z "${WEDI_RC:+"test"}" -a -z "${WEDI_RC+"test"}" ]; then
+    echo "\$WEDI_RC: paramter unset" 1>&2
+    return 3
+  elif [ -z "${WEDI_RC:+"test"}" -a "${WEDI_RC+"test"}" = "test" ]; then
+    echo "\$WEDI_RC: paramter set but null" 1>&2
+    return 4
+  fi
 
-# function to get the absolute path
+  rc_path="$(get_path ${WEDI_RC})"
+  rc_path_retval=${?}
+  if [ ${rc_path_retval} -eq 3 ]; then
+    echo "${rc_path} stored in the \$WEDI_RC (${WEDI_RC}) seems to be a directory" 1>&2
+    echo "This wariable must contain the path of the wedi's RC, A.K.A. runcom, A.K.A. run configuration, ..., file" 1>&2
+    return 5
+  elif [ ${rc_path_retval} -eq 4 ]; then
+    if [ -f ${rc_path} -a -r ${rc_path} -a -w ${rc_path} ]; then
+      if [ -s ${rc_path} ]; then
+        echo "File ${rc_path} is non-zero in size." 1>/dev/null
+      else
+        echo "File ${rc_path} is zero in size." 1>/dev/null
+      fi
+    elif [ ! -e ${rc_path} ]; then
+#      echo "File @ ${rc_path}, read from \$WEDI_RC environment variable (${WEDI_RC}) is non-existing"
+#      echo "Going to touch it..."
+      touch ${rc_path}
+      return
+    fi
+  fi
+  # Let's see the editors on the system. Shall we?
+  scan_editors;
+
+  return 0
+}
+
+check_editor_presence()
+{
+  if command -v ${1} 1>/dev/null 2>&2; then
+    return 0
+  else
+    return 1
+  fi
+
+  exit 1
+}
+
+scan_editors()
+{
+  echo "Scanning editors..."
+
+  for i in {0..7}
+  do
+    if check_editor_presence $(eval echo ${EDITORS[${i}]}); then
+      let "STAT_REG |= $(eval echo \${EDI_${i}})"
+    fi
+  done
+  printf "\nSTATUS REGISTER: %x\n\n" $STAT_REG
+  return 0
+}
+
+open_file()
+{
+  echo "Openning file ${1}..."
+
+  for i in {0..7}
+  do
+    if let "STAT_REG ^ (1 << ${i})"; then
+      eval echo ${EDITORS[${i}]}
+    fi
+  done
+}
+
+
+# function to get the absolute path and a bit more
 get_path()
 {
+#  echo "\$1 received by the get_path function: ${1}"
   if printf "%s" ${1} | grep -e '^-' -q; then
     return 1
-#    continue
+#  elif [ ${1:-'.'} = '.' ]; then
   elif [ ${1:-'.'} = '.' ]; then
     tmp_path="$(pwd)"
   elif [ ${1} = '..' ]; then
     tmp_path="$(echo "$(cd ..; pwd)")"
+  elif printf "%s" ${1} | grep -e '^~' -q; then
+    tmp_path="$(eval dirname "${1}")"
+    tmp_path="${tmp_path}/$(basename "${1}")"
   else
     tmp_path=$(echo "$(cd "$(dirname "${1}")"; pwd)/$(basename "${1}")")
   fi
@@ -146,146 +152,323 @@ evaluate_date()
   return
 }
 
-
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-echo "@                                        @"
-echo "@   Leszek e vajon itten? (^.^)          @"
-echo "@                                        @"
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-init;
-echo "------------------------------------------------------"
-echo "| Az inicializalo fugveny visszateresi erteke: " ${?}
-echo "------------------------------------------------------"
-init_exit_code=${?}
-if [ ${init_exit_code} -eq 0 ]; then
-  echo "sikerult inicializalni"
-else
-  echo "inicializali hibaba futottunk."
-#  show_help
-  echo ${init_exit_code}
-  exit 123
-fi
-
-echo "*********************************"
-echo "*                               *"
-echo "* Remelem eljutok ide...         "
-echo "*  init exit code: ${1}          "
-echo "*                               *"
-echo "*********************************"
-
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo "<                               <"
-echo ">  -kapcsolok parameterek       >"
-echo "< feldolgozasa...               <"
-echo ">                               >"
-echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-
-if printf "%s" ${1} | grep -e "^-[0-9A-za-z]*$" -v -q; then
-  current_path=$(get_path ${1})
-  current_path_retval=${?}
-
-  if [ ${current_path_retval} -eq 3 ]; then
-    echo "${1} is a directory with absolute path: ${current_path}"
-  elif [ ${current_path_retval} -eq 4 ]; then
-    echo "${1} is or will be a file at the following path: ${current_path}"
+call_editor()
+{
+  if [ -z ${1} ]; then
+    read tmp
+    set "${tmp}"
   fi
 
-elif printf "%s" ${1} | grep -e "^-[h]$" -q; then
-  show_help
-  exit 0
+  for i in {0..7}
+  do
+    let "tmp = $STAT_REG & (1 << ${i})"
+    if [ $tmp -gt 0 ]; then
+      eval \${EDITORS[${i}]} \${1}
+      break
+    fi
+  done
 
-elif printf "%s" ${1} | grep -e "^-[mlab]$" -q; then
+  return
+}
+
+push_rc()
+{
+  if [ -z ${1} ]; then
+    read tmp
+    set "${tmp}"
+  fi
+
+  printf "%10d %s\n" $(date +%s) ${1} >> ${rc_path};
+
+  return
+}
+
+hsup_rc()
+{
+  if [ -z ${1} ]; then
+    read tmp
+    set "${tmp}"
+  fi
+
+  local tmp=$(printf "%10d %s\n" $(date +%s) ${1})
+  sed "1 i\\${1}" -i ${rc_path};
+
+  return
+}
+
+pop_rc()
+{
+  local tmp=$(read_rc)
+
+  if [ -z ${tmp} ]; then
+    return 1
+  fi
+
+  del_rc
+
+  return
+}
+
+read_rc()
+{
+  sed -n '$ p' <${rc_path}
+
+  return
+}
+
+del_rc()
+{
+  sed '$ d' -i ${rc_path}
+
+  return
+}
+
+rot_rc()
+{
+  pop_rc | hsup_rc
+
+  return
+}
+
+# function to get a record from wedirc
+# assuming using the thing between ears
+# positional parameters needs to be strictly followed
+# -a|b YY-MM-DD [file absolute path]
+get_by_date()
+{
+  if [ cat $(uname) | grep -i -e 'BSD' ]; then
+    eval epoch=$(date -j -f "${2} %H:%M:%S" "2018-02-02 00:00:00" +%s)
+  elif [ cat $(uname) | grep -i -e 'Linux' ]; then
+    eval epoch=$(date -d="${2}" +%s)
+  else
+    echo "Could not determine system." 1>2
+    exit 6
+  fi
+
   case ${1} in
-    -m|-l)
-      if printf "%s" ${1} | grep -e "^-[0-9A-za-z]*$" -v -q; then
-        current_path=$(get_path ${2})
-        current_path_retval=${?}
 
-      if [ ${current_path_retval} -eq 3 ]; then
-        echo "${2} is a directory with absolute path: ${current_path}"
-      elif [ ${current_path_retval} -eq 4 ]; then
-        echo "${2} is or will be a file at the following path: ${current_path}"
-      fi
+    -a)
+      awk -v epoch=${epoch} '$1 >= epoch { print }' ${3:-'-'}
       ;;
 
-    -a|-b)
-      if evaluate_date "${2}"; then
-      else
-        echo "Wrong date format -- "${2}" --" 1>2
-        echo "Correct format of date: YYYY-MM-DD." 1>2
-        echo "For details on usege use the help: wedi -h" 1>2
+    -b)
+      awk -v epoch=${epoch} '$1 <= epoch { print }' ${3:-'-'}
+      ;;
 
   esac
 
+  return
+}
+
+# function to retrieve a record by the directory
+# the first positional parameter is the absolute path of the directory
+# the second positional parameter is optional as in the get_by_date() function
+# and it is the absolute path of the RC file
+get_by_dir()
+{
+  path=$(get_path $(eval echo \${${1}}))
+  path_retval=${?}
+  if [ ${path_retval} -eq 3 ]; then
+    awk -v path=${path} '$2 ~ /^$path$/ { print  }' ${2:-'-'}
+  fi
+
+  return
+}
+
+# function similar to the get_by_dir() function
+# optins are nearly the same except the firs is the absolute path of a file
+get_by_file()
+{
+  path=$(get_path $(eval echo \${${1}}))
+  path_retval=${?}
+  if [ ${path_retval} -eq 4 ]; then
+    awk -v path=${path} '$3 ~ /^$path$/ { print  }' ${2:-'-'}
+  fi
+
+  return
+}
+
+# counts the number of records
+# first parameter can be a path to a file, if not given STDIN is being used
+# records are being separeted by new-line character
+# the no. of records is being spit to the STDOUT
+cnt_rec_rc()
+{
+  grep -v -e '^[[:blank:]]*#' ${1:-'-'} | wc -l | sed -e 's/^[ \t]*//;s/[ \t]*$//'
+}
+
+init;
+init_return_code=${?}
+
+if [ ${init_return_code} -eq 0 ]; then
+  echo "Successfully initialized. Return Code: ${init_return_code}"
 else
-  echo "Option ${1} not supported. Use wedi -h for addional help on usage." 1>2
-  exit 1
+  echo "Initialization unsuccessful. Return Code: ${init_return_code}"
+  exit ${init_return_code}
 fi
 
-while [ ${1} ]
+echo ">  Processing the options/arguments   >"
+
+
+
+i=1;
+while [ ${i} -le ${#} ]
 do
-  case ${1} in
-    -h)
-      show_help
-      exit 0
+  echo -n "${i} -- ${#} -- "; eval echo \${${i}};
+
+  current_path=$(get_path $(eval echo \${${i}}))
+  current_path_retval=${?}
+
+  echo " "
+  echo ">>>>>>>-- \'${current_path}\' -- \'${current_path_retval}\' --<<<<<<<<<<"
+  echo " "
+
+  case ${current_path_retval} in
+
+    1)
+      eval echo "\${${i}} option receive... OK. The question is: Will it blend?"
+      case "$(eval echo \${${i}})" in
+
+        -m)
+          echo "the MOST called file..."
+          let "j += i";
+          path=$(get_path $(eval echo \${${j}}))
+          path_retval=${?}
+          if [ ${path_retval} -eq 3 ]; then
+            echo "...from directory: ${path}"
+            let "i += 2";
+          elif [ ${path_retval} -eq 4 ]; then
+            echo "...wait a minute, init this a file? ${path}"
+            let "i += 2";
+          elif [ ${path_retval} -eq 1 ]; then
+            echo "...hmmm it seems, that the next positional parameter is an option. ${j}"
+            path=$(get_path '.')
+            echo "using the current working directory... @ ${path}"
+            let "i++";
+          fi
+          ;;
+
+        -l)
+          echo "the LAST called file..."
+          let "j += i";
+          path=$(get_path $(eval echo \${${j}}))
+          path_retval=${?}
+          if [ ${path_retval} -eq 3 ]; then
+            echo "...from directory: ${path}"
+            let "i += 2";
+          elif [ ${path_retval} -eq 4 ]; then
+            echo "...wait a minute, init this a file? ${path}"
+            let "i += 2";
+          elif [ ${path_retval} -eq 1 ]; then
+            echo "...hmmm it seems, that the next positional parameter is an option. ${j}"
+            path=$(get_path '.')
+            echo "using the current working directory... @ ${path}"
+            let "i++";
+          fi
+          ;;
+
+        -a)
+          let "j = i + 1";
+          date=$(eval echo \${${j}})
+#          date_retval=$(evaluate_date ${date}))
+          let "k = i + 2";
+          path=$(get_path $(eval echo \${${k}}))
+          path_retval=${?}
+          if evaluate_date ${date}; then
+            echo "files called AFTER ${date}..."
+            if [ ${path_retval} -eq 3 ]; then
+              echo "...from directory: ${path}"
+              let "i += 3";
+            elif [ ${path_retval} -eq 4 ]; then
+              echo "...wait a minute, init this a file? ${path}"
+              let "i += 3";
+            elif [ ${path_retval} -eq 1 ]; then
+              echo "...hmmm it seems, that the next positional parameter is an option. ${j}"
+              path=$(get_path '.')
+              echo "using the current working directory... @ ${path}"
+              let "i++"
+            fi
+          else
+            echo "wrrrroooonnnnngggggg DATE to list files called AFTER \"${date}\"..."
+            if [ ${path_retval} -eq 3 ]; then
+              echo "...from directory: ${path}"
+              let "i += 3";
+            elif [ ${path_retval} -eq 4 ]; then
+              echo "...wait a minute, init this a file? ${path}"
+              let "i += 3";
+            elif [ ${path_retval} -eq 1 ]; then
+              echo "...hmmm it seems, that the next positional parameter is an option. ${j}"
+              path=$(get_path '.')
+              echo "using the current working directory... @ ${path}"
+              let "i += 2"
+            fi
+          fi
+          ;;
+
+        -b)
+          let "j = i + 1";
+          date=$(eval echo \${${j}})
+#          date_retval=$(evaluate_date ${date}))
+          let "k = i + 2";
+          path=$(get_path $(eval echo \${${k}}))
+          path_retval=${?}
+          if evaluate_date ${date}; then
+            echo "files called BEFORE ${date}..."
+            if [ ${path_retval} -eq 3 ]; then
+              echo "...from directory: ${path}"
+              let "i += 3";
+            elif [ ${path_retval} -eq 4 ]; then
+              echo "...wait a minute, init this a file? ${path}"
+              let "i += 3";
+            elif [ ${path_retval} -eq 1 ]; then
+              echo "...hmmm it seems, that the next positional parameter is an option. ${j}"
+              path=$(get_path '.')
+              echo "using the current working directory... @ ${path}"
+              let "i++"
+            fi
+          else
+            echo "wrrrroooonnnnngggggg DATE to list files called BEFORE \"${date}\"..."
+            if [ ${path_retval} -eq 3 ]; then
+              echo "...from directory: ${path}"
+              let "i += 3";
+            elif [ ${path_retval} -eq 4 ]; then
+              echo "...wait a minute, init this a file? ${path}"
+              let "i += 3";
+            elif [ ${path_retval} -eq 1 ]; then
+              echo "...hmmm it seems, that the next positional parameter is an option. ${j}"
+              path=$(get_path '.')
+              echo "using the current working directory... @ ${path}"
+              let "i += 2"
+            fi
+          fi
+          ;;
+      esac
       ;;
-    -m)
-      if [ -d $(realpath ${2} 2>/dev/null) ]; then
-        echo "most used file in directory $(realpath ${2})"
-        shift 2
-      else
-        echo "most used file"
-        shift 1
-      fi
-      exit 0
-      ;;
-    -l)
-      if [ -d $(realpath ${2}) ]; then
-        echo "list of all files in directory $(realpath ${2})"
-        shift 2
-      else
-        echo "list of all files"
-        shift 1
-      fi
-      ;;
-    -b)
-      if echo ${2} grep -E '^[0-9]{4}\-((((0[13578])|(1[02]))\-(([0-2][0-9])|(3[01])))|(((0[469])|(11))\-(([0-2][0-9])|(30)))|(02\-[0-2][0-9]))$'; then
-        if [ -d $(realpath ${3}) ]; then
-          echo "list of all files before ${2} in folder ${3}"
-          shift 2
-        else
-          echo "list of all files before ${2}"
-          shift 1
+
+    3)
+      eval echo "\${${i}} is a directory with absolute path: ${current_path}"
+      lines=$(cnt_rec_rc ${rc_path} )
+      echo ">>>> ${lines} <<<<"
+      while [ $lines -gt 0 ]
+      do
+        local tmp=$(read_rc)
+        if [ echo ${tmp} | grep -e "${current_path}" -q ]; then
+          echo ${tmp} | awk '{ print $2 }'
         fi
-        else
-          echo "${2}: wrong date format.\nCorrect usage: YYYY-MM-DD." 1>&2
-          exit 6
-      fi
+      rot_rc
+      done
+
+      let "i++";
       ;;
-    -a)
-      if echo ${2} grep -E '^[0-9]{4}\-((((0[13578])|(1[02]))\-(([0-2][0-9])|(3[01])))|(((0[469])|(11))\-(([0-2][0-9])|(30)))|(02\-[0-2][0-9]))$'; then
-        if [ -d $(realpath ${3}) ]; then
-          echo "list of all files after ${2} in folder ${3}"
-          shift 2
-        else
-          echo "list of all files after ${2}"
-          shift 1
-        fi
-        else
-          echo "${2}: wrong date format.\nCorrect usage: YYYY-MM-DD." 1>&2
-          exit 6
-      fi
+
+    4)
+      eval echo "\${${i}} is or will be a file at the following path: ${current_path}"
+      push_rc ${current_path}
+      call_editor ${current_path}
+
+      let "i++";
       ;;
-    *)
-      if [ -d ${1} ]; then
-        echo "directory"
-      elif [ -e ${1} -a -r ${1} -a -w ${1}]; then
-        echo "file exists with rw? rights set"
-      else
-        echo "file does not exists... creation needed"
-      fi
-      ;;
+
   esac
 done
 
